07983f6117e6e040b4bf1df8c23fbd94f8533757
Marcos Vinicius Damaceno <marcosvinicius.mmn@gmail.com>
2023-02-04 22:57:49 -0300

    feat: structure initial project

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+function AboutContainer() {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita magni
+        rem sint numquam natus eligendi laboriosam quisquam voluptates aut,
+        consectetur animi quibusdam delectus optio modi deleniti quae ipsam
+        fuga! Hic.
+      </p>
+    </section>
+  );
+}
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,18 @@
+function ProjectsContainer() {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt soluta
+        reprehenderit dolores porro corporis iste. Pariatur earum eius non iure
+        nobis ad corporis explicabo eaque. Blanditiis eius obcaecati laboriosam
+        id!
+      </p>
+      <a href="#" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+}
+
+export default ProjectsContainer;
